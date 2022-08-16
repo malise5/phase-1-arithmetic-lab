3adb0d8831123f96effab585aaa73b0c1e5fd884
Halkano <kudezx@gmail.com>
2022-08-16 20:10:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1*num2;
+console.log(multiply);
+//Generating a Random number
+const random = Math.ceil(Math.random()*10);
+console.log(random);
+ // modulus
+ let num3 = 29;
+ let num4 = 5;
+ const mod = num3 % num4;
+ console.log(mod);
+ //maximum value
+
+ const max = (5,10,15,20);
+ console.log(Math.max(max));
